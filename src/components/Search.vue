8b3af9724897335cf4e0ecf14f7e801eea1da06a
Sebastian <sebastianwuczkowski@gmail.com>
2023-03-24 15:32:19 +0100

    Add Search Component

---
+++ src/components/Search.vue
@@ -0,0 +1,19 @@
+<template >
+    <div class="field is-grouped">
+  <p class="control is-expanded">
+    <input class="input" type="text" placeholder="Find your notes by title">
+  </p>
+  <p class="control">
+    <a class="button is-info">
+      Clean 
+    </a>
+  </p>
+</div>
+    </template>
+    
+    <script setup>
+    </script>
+    
+    <style>
+    
+    </style>

bbca44a53e6123e269849356ea7c56385698fbe4
Sebastian <sebastianwuczkowski@gmail.com>
2023-04-12 20:41:45 +0200

    Stats page

--- src/views/Stats.vue
+++ src/views/Stats.vue
@@ -1,5 +1,10 @@
 <template>
-    <h1>Stats</h1>
+  <div class="m-6	 has-text-centered	">
+    <h1 class="is-size-1">Work in progress!</h1>
+    <h1 class="is-size-2	">Expect the best</h1>
+    <progress class="progress is-primary mt-6 mb-2" value="15" max="100">15%</progress>
+    <span>Progress bar</span>
+  </div>
   </template>
   
   <script>

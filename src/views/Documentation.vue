e248ea75e0824c9d52b1aa616e921cc1db37662a
Sebastian <sebastianwuczkowski@gmail.com>
2023-04-12 20:41:12 +0200

    Documentation page

---
+++ src/views/Documentation.vue
@@ -0,0 +1,35 @@
+<template>
+        
+        <div class="mt-6	">
+    <h2 class="is-size-1  has-text-centered">Documentation</h2>
+    <p class="is-size-5 mt-5	">This is part of Sebastian Wuczkowski <strong>portfolio</strong> If you want to check more please check Github or Linkedin. This project was created from scratch based on external libralers! The whole project was created as a inspiration of one Youtube video. </p>
+  </div>
+  <p class="is-size-5 mt-5">In this project there were used:</p>
+  <div class="content is-medium mb-6">
+    <ul>
+    <li>Pinia</li>
+    <li>Vue 3</li>
+    <li>Composition API</li>
+    <li>BULMA</li>
+    <li>FireBase</li>
+    <li><i class="fa-solid fa-heart" style="color: #fb0909;"></i> to VUE</li>
+  </ul>
+  <p class="is-size-5 mt-5	">The whole project was created by Vite. In near future there is idea to add: </p>
+  <ul>
+    <li>Unit test</li>
+    <li>Some stats created in: Chart.js</li>
+    <li>TypeScript</li>
+  </ul>
+  </div>
+ 
+ 
+  </template>
+  
+  <script>
+import { looseIndexOf } from '@vue/shared';
+
+  </script>
+  
+  <style>
+  
+  </style>
